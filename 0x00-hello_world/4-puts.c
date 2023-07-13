9db3fd22c93eee0116e0511d818ba255165434b1
zahraa-khalil <zahraa.khalil2020@gmail.com>
2023-07-13 14:20:35 -0700

    update--

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,8 @@
 
 
 /**
- * main - prints "Programming is like building a multilingual puzzle, followed by a newline.
+ * main - prints "Programming is like building a multilingual
+ * puzzle, followed by a newline.
  * Return: Always 0.
  */
 int main(void)
