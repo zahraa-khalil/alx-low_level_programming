0c63e3705551792ae8da2625e3d813ced3e75ba8
zahraa-khalil <zahraa.khalil2020@gmail.com>
2023-07-13 13:50:59 -0700

    update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 
-/* Entry point of the program.*/
-
+/**
+ * Entry point of the program.
+ *
+ * @return 0 upon successful execution.
+ */
 int main(void)
-{/*function main*/
+{
+	// Your code here
 	const char *string = "\"Programming is like building a multilingual puzzle";
 
 	puts(string);
