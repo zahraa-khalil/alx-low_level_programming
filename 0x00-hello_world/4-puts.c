794d613cdc48f1c265dccf59c7d8aa20b8bea653
zahraa-khalil <zahraa.khalil2020@gmail.com>
2023-07-13 13:38:30 -0700

    petty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
-int main(void) {
-    const char* string="\"Programming is like building a multilingual puzzle";
-    puts(string);
-    return 0;
+int main(void) 
+{
+	const char* string = "\"Programming is like building a multilingual puzzle";
+	puts(string);
+	return (0);
 }
 
