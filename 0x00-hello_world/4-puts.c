0bdb214cb503fa543c9f9c3a7011b3e37edb9468
zahraa-khalil <zahraa.khalil2020@gmail.com>
2023-07-13 13:33:15 -0700

    update-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main(void) {
-    const char* string="Programming is like building a multilingual puzzle";
+    const char* string="\"Programming is like building a multilingual puzzle";
     puts(string);
     return 0;
 }
