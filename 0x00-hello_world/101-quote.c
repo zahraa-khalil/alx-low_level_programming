4b5fdb298c60d3f5772bf4f4897449cbd018e5c1
zahraa-khalil <zahraa.khalil2020@gmail.com>
2023-07-13 17:23:07 -0700

    update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <unistd.h>
 
 /**
@@ -9,7 +8,9 @@
 int main(void)
 
 {
-	fput("and that piece of art is useful\" - Dora	Kor
-par, 2015-10-19\n", stdout);
+	write(2,
+	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	      59);
+
 	return (1);
 }
