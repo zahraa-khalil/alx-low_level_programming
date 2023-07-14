e90bcd9e94e92508a23685da8a4afd2656b8f16e
zahraa-khalil <zahraa.khalil2020@gmail.com>
2023-07-14 16:46:19 -0700

    101

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -13,20 +13,22 @@ int main(void)
 	{
 		for (j = n + 1; j <= 9; j++)
 		{
-			for ( i = j + 1; i <= 9; i++)
+			for (i = j + 1; i <= 9; i++)
 			{
 				putchar('0' + n);
-                        	putchar('0' + j);
+				putchar('0' + j);
 				putchar('0' + i);
-                        	if (n == 7 && j == 8 && i ==9)
-                        	{
-                                	continue;
-                        	}
+				if (n == 7 && j == 8 && i == 9)
+				{
+					continue;
+				}
+
 				putchar(',');
-                        	putchar(' ');
+				putchar(' ');
 			}
 		}
 	}
+
 	putchar('\n');
 
 	return (0);
