ba5800c1797fe0e1711fabd36209ac04ae4b3c9a
zahraa-khalil <zahraa.khalil2020@gmail.com>
2023-07-14 13:30:04 -0700

    update

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *main - print numbers in base 10, followed by a new line.
+ *Return: Always numbers in base 10, followed by a new line
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		putchar(n + '0');
+		putchar('\n');
+	}
+
+	return (0);
+}
