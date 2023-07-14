49e5a7903c197e6cfa8721ca50f35d30250c7ad0
zahraa-khalil <zahraa.khalil2020@gmail.com>
2023-07-13 17:06:35 -0700

    up

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,13 @@
 #include <unistd.h>
 
 /**
- * main - A C  prints and that piece of art is useful" - Dora Korpar, 2015-10-19 , followed by a new line.
+ * main - A C prints and that piece of art is useful" - Dora Korpar, 2015-1
+ * 0-19 , followed by a new line.
  * Return: Always 1
  */
 int main(void)
 
 {
-	fputs("that piece of art is useful\n- Dora Korpar, 2015-10-19\n", stdout);
+	fputs("and that piece of art is useful\n- Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
