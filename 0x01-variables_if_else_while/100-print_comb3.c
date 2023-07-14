686800ab0e3cc674a73947bd211edf57746c912a
zahraa-khalil <zahraa.khalil2020@gmail.com>
2023-07-14 15:30:55 -0700

    update-100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ *main - print the alphabet in lowercase, followed by a new line.
+ *Return: Always alphabet in lowercase, followed by a new line
+ */
+
+int main(void)
+{
+	int n, j;
+
+	for (n = 0; n <= 9; n++)
+	{
+		for (j = n + 1; j <= 9; j++)
+		{
+			putchar('0' + n);
+			putchar('0' + j);
+			putchar(',');
+                        putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
