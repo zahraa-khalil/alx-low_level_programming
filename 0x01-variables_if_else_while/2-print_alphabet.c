912bf210b1aa2ff744288c35382c28d0345246aa
zahraa-khalil <zahraa.khalil2020@gmail.com>
2023-07-14 12:35:30 -0700

    putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print the alphabet in lowercase, followed by a new line.
+ * Return: Always alphabet in lowercase, followed by a new line
+ */
+
+int main(void) {
+    char ch;
+
+    for (ch = 'a'; ch <= 'z'; ch++) {
+        putchar(ch);
+    }
+
+    putchar('\n');
+
+    return 0;
+}
