c51f634613abd79636cc3e7e39887421854b5131
zahraa-khalil <zahraa.khalil2020@gmail.com>
2023-07-14 17:02:59 -0700

    -final

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ *main - print the alphabet in lowercase, followed by a new line.
+ *Return: Always alphabet in lowercase, followed by a new line
+ */
+
+int main(void)
+{
+	int n, j;
+
+	for (n = 10; n <= 99; n++)
+	{
+		for (j = n + 1; j <= 99; j++)
+		{
+			putchar('0' + n / 10);
+			putchar('0' + n % 10);
+			putchar(' ');
+			putchar('0' + j / 10);
+			putchar('0' + j % 10);
+			if (n == 98 && j == 99)
+			{
+				continue;
+			}
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
