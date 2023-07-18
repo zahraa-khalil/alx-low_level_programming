c116c32a9c8552f1a3ba12cc306d7ef7144aae39
zahraa-khalil <zahraa.khalil2020@gmail.com>
2023-07-18 10:37:32 -0700

    beatify

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,7 +7,7 @@
 
 int main(void)
 {
-       	_putchar('_');
+	_putchar('_');
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
@@ -15,7 +15,7 @@ int main(void)
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
-        _putchar('\n');
+	_putchar('\n');
 
-        return (0);
+	return (0);
 }
