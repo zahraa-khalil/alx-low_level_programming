b4494183be610c1febb5f949c7af3592e558de2f
zahraa-khalil <zahraa.khalil2020@gmail.com>
2023-07-18 06:35:02 -0700

    add puchar 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ *main - print _puchar, followed by a new line.
+ *Return: Always _puchar, followed by a new line
+ */
+
+int main(void)
+{
+       	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+        putchar('\n');
+
+        return (0);
+}
