4e07fa715d4fe893123c4e8d5dee9fc9e1507dbc
zahraa-khalil <zahraa.khalil2020@gmail.com>
2023-07-20 13:16:24 +0300

    update  1st task -4

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -8,7 +8,7 @@
 
 int _isupper(int c)
 {
-    if ((c >= 'A' && c <= 'Z') || (c >= 66 && c <= 90))
+    if ((c >= 'A' && c <= 'Z') || (c >= 65 && c <= 90))
         return (1);
     else
         return (0);
