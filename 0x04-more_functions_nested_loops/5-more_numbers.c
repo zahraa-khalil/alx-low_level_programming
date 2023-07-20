2fbec00da9af1346af43bc2f81dc46a0d1ee0fd4
zahraa-khalil <zahraa.khalil2020@gmail.com>
2023-07-20 23:35:16 +0300

    up1

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- *more_numbers - prints the numbers,from 0 to 14, followed by a new line.
- *Return: Always nothing
- */
+*more_numbers - prints the numbers,from 0 to 14, followed by a new line.
+*Return: Always nothing
+*/
 void more_numbers(void)
 {
 	int i, j;
