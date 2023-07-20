900a3dd7894b3cd31f202de6eeaea6b3fe00e86d
zahraa-khalil <zahraa.khalil2020@gmail.com>
2023-07-20 12:58:08 +0300

    update  1st task

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,9 +2,10 @@
 
 /**
  *main - check for uppercase character.
- *@n: number to be checked
+ *@n: charcter to be checked
  *Return: Always 1 if c is uppercase, 0 otherwise.
  */
+
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
