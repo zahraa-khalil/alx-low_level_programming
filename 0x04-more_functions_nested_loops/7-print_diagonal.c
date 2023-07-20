0d29f03bbc359e93e39e3218730c32570b5fdea6
zahraa-khalil <zahraa.khalil2020@gmail.com>
2023-07-20 16:40:16 +0300

    update diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ *print_line - prints a diagonal line in the terminal
+ *@n: int parameter
+ *Return: Always void
+ */
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n > 0)
+	{
+		for (i = 1; i <= n; i++)
+		{
+			for (j = 1; j <= i; j++)
+			{
+				_putchar(' ');
+			}
+
+			_putchar('\\');
+			_putchar('\n');
+		}
+
+		_putchar('\n');
+	}
+	else
+		_putchar('\n');
+}
