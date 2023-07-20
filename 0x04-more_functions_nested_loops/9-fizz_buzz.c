03ec00c23ae52a12694a39dfbe2a822c786cbd58
zahraa-khalil <zahraa.khalil2020@gmail.com>
2023-07-20 23:47:18 +0300

    up9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,7 +10,7 @@ int main(void)
 {
 	int i;
 
-	for (i = 1; i < 100; i++)
+	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0)
 			printf("Fizz ");
@@ -20,6 +20,9 @@ int main(void)
 			printf("FizzBuzz ");
 		else
 			printf("%d ", i);
+        if (i == 100)
+			continue;
+		printf(" ");
 	}
 
 	return (0);
