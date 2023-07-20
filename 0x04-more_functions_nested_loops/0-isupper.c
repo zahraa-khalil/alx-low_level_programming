0e628bc0822a909dc1bae37c17806fb5b2eceb24
zahraa-khalil <zahraa.khalil2020@gmail.com>
2023-07-20 13:17:16 +0300

    update  1st task comment

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - check for uppercase character.
+ * _isupper - check for uppercase character.
  * @c: charcter to be checked
  * Return: Always 1 if c is uppercase, 0 otherwise.
  */
