def6b5a2d3d43ef4f31a51c4494cbb7f8c2312f5
zahraa-khalil <zahraa.khalil2020@gmail.com>
2023-07-25 00:34:53 +0300

    1$

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *print_rev - function that prints a string, in reverse,
+ *followed by a new line.
+ *@s: char to be checked
+ *Return: nothing
+ */
+
+void print_rev(char *s)
+{
+	int length = 0;
+
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+
+	while (length >= 0)
+	{
+		_putchar(s[length]);
+		length--;
+	}
+
+	_putchar('\n');
+}
