e7ddd065015ba8e70098546faf4263e41919ef68
zahraa-khalil <zahraa.khalil2020@gmail.com>
2023-07-25 03:39:02 +0300

    1$

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -28,11 +28,10 @@ void puts_half(char *str)
 	else
 	{
 		n = (length - 1) / 2;
-		for(i = n; i < length; i++)
+		for (i = n; i < length; i++)
 		{
 			_putchar(str[i]);
 		}
-		
 	}
 
 	_putchar('\n');
