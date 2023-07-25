565beac77125243c02899accaea626401e4c187b
zahraa-khalil <zahraa.khalil2020@gmail.com>
2023-07-26 00:44:17 +0300

    1$

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -16,7 +16,7 @@ int _atoi(char *s)
 	{
 		if (s[i] >= '0' && s[i] <= '9')
 		{
-			result = result *10 + (s[i] - '0');
+			result = result * 10 + (s[i] - '0');
 			return (result);
 		}
 		else
@@ -25,5 +25,5 @@ int _atoi(char *s)
 		}
 	}
 
-	return 0;
+	return (0);
 }
