62901f14aade8db2272471bf93de1233109a4e06
zahraa-khalil <zahraa.khalil2020@gmail.com>
2023-07-26 02:02:12 +0300

    1$

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -16,7 +16,7 @@ int _atoi(char *s)
     {
         if (s[i] >= '0' && s[i] <= '9')
         {
-            result = ((s[i + 0] - '0'));
+            result = ((s[i + 1] - '0'));
             printf("%d", result);
         }
         else
