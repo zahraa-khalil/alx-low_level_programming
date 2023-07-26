8be0755753c7372c03fe1d54db383ba35667d343
zahraa-khalil <zahraa.khalil2020@gmail.com>
2023-07-26 03:17:52 +0300

    1$

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -21,7 +21,7 @@ int _atoi(char *s)
 
 	while (s[i] != '\0')
 	{
-		if (s[i] >= '0' && s[i] <= '9' && s[i - 1] != '-')
+		if (s[i] >= '0' && s[i] <= '9')
 		{
 			result = result * 10 + (s[i] - '0');
 			i++;
