a2cd5f678e07ee5d53f1430524cedf0b4ac07e19
zahraa-khalil <zahraa.khalil2020@gmail.com>
2023-07-28 20:16:09 +0300

    1$

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,18 +11,10 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
-	int j = 0;
 
-	while (dest[i] != '\0')
+	while (src[i] != '\0' && i < n)
 	{
-		dest[i] = dest[i];
-		i++;
-	}
-
-	while (src[j] != '\0' && j < n)
-	{
-		dest[i + j] = src[j];
-		j++;
+		dest[i] = src[i];
 	}
 
 	return (dest);
