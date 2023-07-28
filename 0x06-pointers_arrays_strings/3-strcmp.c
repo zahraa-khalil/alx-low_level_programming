acab48d92541a0ef0a7acc4c8a22a02532e93430
zahraa-khalil <zahraa.khalil2020@gmail.com>
2023-07-28 20:09:31 +0300

    1$

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ **_strncpy -function that concatenates two strings.
+ *@dest: Pointer to the destination buffer where the result will be stored.
+ *@src: Pointer to the source string that will be appended to dest.
+ *@n: int n bytes from src.
+ *Return: a pointer to the resulting string dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i] != '\0')
+	{
+		dest[i] = dest[i];
+		i++;
+	}
+
+	while (src[j] != '\0' && j < n)
+	{
+		dest[i + j] = src[j];
+		j++;
+	}
+
+	return (dest);
+}
