37416bdf15884f6bd7dec403361c78a2f9a5d611
zahraa-khalil <zahraa.khalil2020@gmail.com>
2023-07-28 23:27:10 +0300

    1$

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,11 +9,10 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0;
-
-	while (s1[i] != '\0' && s1[i] == s2[i])
+	while (*s1 != '\0' && *s1 == *s2)
 	{
-		i++;
+		s1++;
+		s2++;
 	}
 
 	return (*s1 - *s2);
