5d0a2747ac9f4b7d4fa1ee889412882a3b053646
zahraa-khalil <zahraa.khalil2020@gmail.com>
2023-08-03 01:16:56 +0300

    1-rec

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ **_puts_recursion -function that prints a string, followed by a new line.
+ *@s: Pointer.
+ *Return: void.
+ */
+
+void _puts_recursion(char *s)
+{
+	int i = 0;
+
+    while(s[i] != '\0')
+    {
+           _putchar(s[i]);
+           i++;
+           
+    }
+ 
+     _putchar('\n');
+}
