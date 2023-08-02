f4249e23855274d6053da2867c66e78ca1dd369a
zahraa-khalil <zahraa.khalil2020@gmail.com>
2023-08-03 01:18:08 +0300

    1-rec

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -10,12 +10,11 @@ void _puts_recursion(char *s)
 {
 	int i = 0;
 
-    while(s[i] != '\0')
-    {
-           _putchar(s[i]);
-           i++;
-           
-    }
- 
-     _putchar('\n');
+	while (s[i] != '\0')
+	{
+		_putchar(s[i]);
+		i++;
+	}
+
+	_putchar('\n');
 }
