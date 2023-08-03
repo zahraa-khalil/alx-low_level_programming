7b66c61b2cb2a3263187a45b98fa4741d07775d2
zahraa-khalil <zahraa.khalil2020@gmail.com>
2023-08-03 04:02:35 +0300

    1-rec

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *_print_rev_recursion - function that prints a string in reverse.
+ *@s: Pointer.
+ *Return: void.
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
