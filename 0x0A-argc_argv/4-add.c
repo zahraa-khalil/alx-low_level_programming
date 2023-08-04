faaffd35f76ee154da0bcd6306fc4a81808b3ae4
zahraa-khalil <zahraa.khalil2020@gmail.com>
2023-08-05 02:17:20 +0300

    update

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -34,7 +34,10 @@ int main(int argc, char *argv[])
 
 		for (i = 1; i < argc; i++)
 		{
-			sum += atoi(argv[i]);
+			if (atoi(argv[i]) >= 0)
+			{
+				sum += atoi(argv[i]);
+			}
 		}
 
 		printf("%d\n", sum);
