6a220311d2f5c3c774662beecbfb0f1687bbcddc
zahraa-khalil <zahraa.khalil2020@gmail.com>
2023-08-05 01:02:56 +0300

    update

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ *main - print the alphabet in lowercase, followed by a new line.
+ *Return: Always alphabet in lowercase, followed by a new line
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+    printf("%s\n", argv[0]);
+}
