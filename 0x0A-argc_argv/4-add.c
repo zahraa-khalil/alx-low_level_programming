edcf3019299420e95529e02645743f516eec1c13
zahraa-khalil <zahraa.khalil2020@gmail.com>
2023-08-05 02:22:55 +0300

    update

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -10,40 +10,40 @@
 
 int main(int argc, char *argv[])
 {
-    int sum = 0;
+	int sum = 0;
 
-    int i, j, positive_num;
+	int i, j, positive_num;
 
-    if (argc <= 1)
-    {
-        printf("0\n");
-        return (1);
-    }
-    else
-    {
-        for (i = 1; i < argc; i++)
-        {
-            for (j = 0; argv[i][j] != '\0'; j++)
-            {
-                if (argv[i][j] < '0' || argv[i][j] > '9')
-                {
-                    printf("Error\n");
-                    return (1);
-                }
-            }
-        }
+	if (argc <= 1)
+	{
+		printf("0\n");
+		return (1);
+	}
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			for (j = 0; argv[i][j] != '\0'; j++)
+			{
+				if (argv[i][j] < '0' || argv[i][j] > '9')
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+		}
 
-        for (i = 1; i < argc; i++)
-        {
-            positive_num = atoi(argv[i]);
-            if (positive_num >= 0)
-            {
-                sum += atoi(argv[i]);
-            }
-        }
+		for (i = 1; i < argc; i++)
+		{
+			positive_num = atoi(argv[i]);
+			if (positive_num >= 0)
+			{
+				sum += atoi(argv[i]);
+			}
+		}
 
-        printf("%d\n", sum);
-    }
+		printf("%d\n", sum);
+	}
 
-    return (0);
+	return (0);
 }
