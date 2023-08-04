55b6338d05e001d6cffbe72afddaa7eef1c4ef9d
zahraa-khalil <zahraa.khalil2020@gmail.com>
2023-08-05 02:44:42 +0300

    update puts

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -2,7 +2,7 @@
 
 /**
  *_puts - function that prints a string, followed by a
- * new line
+ *new line
  *@str: char to be checked
  *Return: nothing
  */
@@ -11,11 +11,12 @@ void _puts(char *str)
 {
 	int i = 0;
 
-    while (str[i] != '\0') {
-        putchar(str[i]);
-        i++;
-    }
+	while (str[i] != '\0')
+	{
+		putchar(str[i]);
+		i++;
+	}
 
-    putchar('\n'); 
-    return i + 1;
+	putchar('\n');
+	return (i + 1);
 }
