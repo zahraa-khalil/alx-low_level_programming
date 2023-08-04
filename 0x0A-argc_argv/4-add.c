eb72c03f3fdfdd45f13541ec78b6ef5e51f4db3b
zahraa-khalil <zahraa.khalil2020@gmail.com>
2023-08-05 02:13:46 +0300

    update

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - program that adds positive numbers.
+ *@argc: argc parameter
+ *@argv: an array of a command listed
+ *Return: 0 for success
+ */
+
+int main(int argc, char *argv[])
+{
+	int sum = 0;
+	int i, j;
+
+	if (argc <= 1)
+	{
+		printf("0\n");
+		return (1);
+	}
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			for (j = 0; argv[i][j] != '\0'; j++)
+			{
+				if (argv[i][j] < '0' || argv[i][j] > '9')
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+		}
+
+		for (i = 1; i < argc; i++)
+		{
+			sum += atoi(argv[i]);
+		}
+
+		printf("%d\n", sum);
+	}
+
+	return (0);
+}
