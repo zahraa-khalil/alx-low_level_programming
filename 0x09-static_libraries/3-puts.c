b833f3d404e4f2ff86b3c6484a2bc477681a3a0c
zahraa-khalil <zahraa.khalil2020@gmail.com>
2023-08-05 00:31:21 +0300

    del main.c

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,20 +1,13 @@
 #include "main.h"
 
 /**
- *_puts -  function that prints a string, followed by a new line
+ *_puts - function that prints a string, followed by a
+ * new line
  *@str: char to be checked
  *Return: nothing
  */
 
 void _puts(char *str)
 {
-	int i = 0;
 
-	while (str[i] != '\0')
-	{
-		_putchar(str[i]);
-		i++;
-	}
-
-	_putchar('\n');
 }
