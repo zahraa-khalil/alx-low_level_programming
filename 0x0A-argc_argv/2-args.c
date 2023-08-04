e7aa0c8f29da9c470a5a02e8d4b2375d0cd63b95
zahraa-khalil <zahraa.khalil2020@gmail.com>
2023-08-05 01:19:07 +0300

    update

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -9,13 +9,13 @@
 
 int main(int argc, char *argv[])
 {
-    int i = 0;
+	int i = 0;
 
-    while (i < argc)
-    {
-        printf("%s\n", argv[i]);
-        i++;
-    }
-	
+	while (i < argc)
+	{
+		printf("%s\n", argv[i]);
+		i++;
+	}
+
 	return (0);
 }
