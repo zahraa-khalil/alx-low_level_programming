a5cc2aec22f70375781a63598e0dfac71b87b7b6
zahraa-khalil <zahraa.khalil2020@gmail.com>
2023-08-05 01:29:51 +0300

    --3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - program that print the result of the multiplication
+ *@argc: argc parameter
+ *@argv: an array of a command listed
+ *Return: 0 for success
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc <= 2)
+	{
+		printf("Error");
+		return (1);
+	}
+	else
+	{
+		int multi = atoi(argv[1]) * atoi(argv[2]);
+
+		printf("%d\n", multi);
+	}
+
+	return (0);
+}
