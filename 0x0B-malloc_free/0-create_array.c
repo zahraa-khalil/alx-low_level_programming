46dd8fe549fa3ee523c3e59287e9f53c17c011ce
zahraa-khalil <zahraa.khalil2020@gmail.com>
2023-08-08 16:42:07 +0300

    handle env ++ 1st task

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ **create_array - function that creates an array of chars, and initializes
+ *it with a specific char.
+ *@c: character to be initialized with.
+ *@size: size of the array
+ *Return: a pointer or null on failure
+ */
+
+char *create_array(unsigned int size, char c)
+{
+    char *arr;
+    unsigned int i;
+
+    if (size > 0)
+    {
+        arr = (char *)malloc(size * sizeof(char));
+        // pointer now points to the memory location
+        // where space for size chars has been allocated on the heap.
+        if (arr != NULL)
+        {
+            // assign c to first char in the array
+            for (i = 0; i < size; i++)
+            {
+                arr[i] = c;
+            }
+        }
+        else
+        {
+            return (NULL);
+        }
+
+        return arr;
+    }
+    else
+    {
+        return (NULL);
+    }
+}
