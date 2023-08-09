0eb38e0653a5b112c0cfdb537f83b522bfc17fbc
zahraa-khalil <zahraa.khalil2020@gmail.com>
2023-08-09 03:16:21 +0300

    1-str

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,48 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+
+/**
+ **create_array - function that returns a pointer to a newly allocated space in memory
+ *it with a specific char.
+ *@str: string to allocate
+ *Return: a pointer or null on failure
+ */
+
+char *_strdup(char *str)
+{
+	int size = 0;
+	char *p = NULL;
+	char *dup = NULL;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		p = str;
+		while (*p++)
+		{
+			size++;
+		}
+
+		dup = malloc((size + 1));
+
+		if (dup != NULL)
+		{
+			p = dup;
+			while (*str)
+			{ 	*p++ = *str++;
+			}
+
+			*p = 0;
+			return (dup);
+		}
+		else
+		{
+			return (NULL);
+		}
+	}
+}
