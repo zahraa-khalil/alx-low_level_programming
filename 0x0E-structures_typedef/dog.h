ec226ef5928ff15308eed57e64f09920fb4cf423
zahraa-khalil <zahraa.khalil2020@gmail.com>
2023-08-16 01:40:47 +0300

    update-dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,6 @@
 #ifndef DOG_H
 #define DOG_H
 
-
 /**
  *struct dog - dog structure
  *@name structure member for dog name
@@ -15,6 +14,10 @@ struct dog {
     char *owner;
 };
 
-
+typedef struct Dog{
+    char *name[50];
+    float age;
+    char *owner[50];
+}  my_dog;
 
 #endif
