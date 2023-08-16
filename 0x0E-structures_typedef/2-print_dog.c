05bcd092b6851d9ed9773c1f7d26232ffae22d82
zahraa-khalil <zahraa.khalil2020@gmail.com>
2023-08-16 18:11:48 +0300

    update-

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,13 +8,12 @@
  */
 void print_dog(struct dog *d)
 {
-    char nil[] = "(nil)";
+	char nil[] = "(nil)";
 
-    if (d != NULL)
-    {
-        printf("Name: %s\n", d->name?d->name : nil );
-        printf("Age: %f\n", d->age);
-        printf("Owner: %s\n", d->owner? d->owner : nil);
-    }
-  
+	if (d != NULL)
+	{
+		printf("Name: %s\n", d->name ? d->name : nil);
+		printf("Age: %f\n", d->age);
+		printf("Owner: %s\n", d->owner ? d->owner : nil);
+	}
 }
