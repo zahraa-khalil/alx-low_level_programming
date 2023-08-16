6b4cabbcd9c20b4cc4e7805e1d07e1bac5c70300
zahraa-khalil <zahraa.khalil2020@gmail.com>
2023-08-16 23:49:32 +0300

    update-freedog

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -3,13 +3,23 @@
 #include <stdio.h>
 
 /**
- *dog_t *new_dog - function that creates a new dog.
- *@name: structure member for dog name
- *@age: structure member for dog age
- *@owner: - structure member for dog owner
- * Return: NULL if the function fails
+ *free_dog - function that frees dogs.
+ *@d: dog
  */
 void free_dog(dog_t *d)
 {
-  
+	if (d != NULL)
+	{
+		if (d->name != NULL)
+		{
+			free(d->name);
+		}
+
+		if (d->owner != NULL)
+		{
+			free(d->owner);
+		}
+
+		free(d);
+	}
 }
