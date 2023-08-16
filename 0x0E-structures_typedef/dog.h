5290089d1daea46b994e5297ea4a757753c11825
zahraa-khalil <zahraa.khalil2020@gmail.com>
2023-08-16 18:19:54 +0300

    update

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,6 +15,13 @@ typedef struct dog
 	char *owner;
 } my_dog;
 
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
