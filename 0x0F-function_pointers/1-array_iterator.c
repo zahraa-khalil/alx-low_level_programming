7b7542c3b2f149f174c48266aef6d5b04f00d78c
zahraa-khalil <zahraa.khalil2020@gmail.com>
2023-08-17 16:42:33 +0300

    array-iterate

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+
+/**
+ *print_name- function that prints a name.
+ *@action: Pointer to function
+ *@array: array of pointers
+ *@size: size of array
+ *Returns: nothing
+ */
+void array_iterator(int *array, size_t size, void(*action)(int))
+{
+	unsigned int i = 0;
+
+	while (i < size)
+	{
+		action(array[i]);
+		i++;
+	}
+}
