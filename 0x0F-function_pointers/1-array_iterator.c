9037fb4e548d4937a3eb7be04fcd39a92fcfe1e4
zahraa-khalil <zahraa.khalil2020@gmail.com>
2023-08-17 17:52:42 +0300

    array-iterate

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,9 +11,9 @@ void array_iterator(int *array, size_t size, void(*action)(int))
 {
 	unsigned int i = 0;
 
-	if (action != NULL)
+	if (action != NULL && array != NULL)
 	{
-		while (i < size && size != 0)
+		while (i < size)
 		{
 			action(array[i]);
 			i++;
