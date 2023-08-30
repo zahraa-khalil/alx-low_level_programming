ee3f139b449ba7b7e378811410e8744fde59825e
Fatma Khaleel <FKhaleel@mufawid.net>
2023-08-30 03:46:34 +0300

    update_2

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -12,11 +12,7 @@ size_t print_listint(const listint_t *h)
 
 	while (h != NULL)
 	{
-		if (count <= h->n)
-		{
-			printf("%d\n", h->n);
-		}
-
+		printf("%d\n", h->n);
 		h = h->next;
 		count++;
 	}
