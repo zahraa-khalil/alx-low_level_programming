0903517989b12294d498b2404a91e7ab909cf45d
zahraa-khalil <zahraa.khalil2020@gmail.com>
2023-10-03 04:09:11 +0300

    update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- *read_textfile- reads a text file and prints it to the POSIX
+ *create_file- reads a text file and prints it to the POSIX
  *@text_content: NULL terminated string to write to the file
  *@filename: name of the file to create
  *Return: actual number of letters it could read and print or 0
@@ -14,7 +14,6 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	/*Open the file for writing, create if it doesn't exist, truncate to 0 length if it does */
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
 
 	if (fd == -1)
