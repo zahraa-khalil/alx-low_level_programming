7e5c182836c1d652a9f1983d87e5dd9656d3288b
zahraa-khalil <zahraa.khalil2020@gmail.com>
2023-10-26 22:57:05 +0300

    update_null

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -12,6 +12,9 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int decimal;
 	unsigned int i;
 
+	if (b == NULL)
+		return (0);
+
 	for (decimal = 0, i = 0; b[i] != '\0'; i++)
 	{
 		if (b[i] == '1')
