5aef7a83c7f7a4712fa85171a17d7c3974ea84c8
zahraa-khalil <zahraa.khalil2020@gmail.com>
2023-12-07 22:55:08 +0200

    return_numOfElements

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,21 +1,21 @@
 #include "lists.h"
 
 /**
- * dlistint_len - function that returns the number of elements
- * in a linked dlistint_t list.
- * @h: a pointer
- * Return: the number of nodes
+ *dlistint_len - function that returns the number of elements
+ *in a linked dlistint_t list.
+ *@h: a pointer
+ *Return: the number of nodes
  */
 
 size_t dlistint_len(const dlistint_t *h)
 {
-    size_t i = 0;
+	size_t i = 0;
 
-    while (h)
-    {
-        h = h->next;
-        i++;
-    }
+	while (h)
+	{
+		h = h->next;
+		i++;
+	}
 
-    return (i);
+	return (i);
 }
