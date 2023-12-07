a4fed5beada96726bddeb6522afeeaad40d4db07
zahraa-khalil <zahraa.khalil2020@gmail.com>
2023-12-08 00:57:33 +0200

    index

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- *get_dnodeint_at_index - function that returns the sum of all the data
+ *sum_dlistint - function that returns the sum of all the data
  *(n) of a dlistint_t linked list.
  *@head: a pointer
  *Return: sum
