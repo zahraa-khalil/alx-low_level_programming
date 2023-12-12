68624fbd6297956e2ace1a245d9367afe83f4069
zahraa-khalil <zahraa.khalil2020@gmail.com>
2023-12-12 15:47:59 +0200

    add-puts

---
+++ 0x18-dynamic_libraries/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *_puts - function that prints a string, followed by a
+ *new line
+ *@str: char to be checked
+ *Return: nothing
+ */
+
+void _puts(char *str)
+{
+	_putchar(str[0]);
+}
