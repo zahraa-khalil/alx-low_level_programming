ffe4dcdc92c6aef5869783396b53b227392fb589
zahraa-khalil <zahraa.khalil2020@gmail.com>
2024-06-07 05:40:17 +0300

    handle-style

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -37,14 +37,3 @@ def island_perimeter(grid):
                     perimeter += 1
 
     return perimeter
-
-
-if __name__ == "__main__":
-    grid = [
-        [0, 0, 0, 0, 0, 0],
-        [0, 1, 0, 0, 0, 0],
-        [0, 1, 0, 0, 0, 0],
-        [0, 1, 1, 1, 0, 0],
-        [0, 0, 0, 0, 0, 0]
-    ]
-    print(island_perimeter(grid))  # Expected Output: 12
