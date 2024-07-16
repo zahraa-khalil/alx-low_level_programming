9d09d142a458b6d59bbe1a87c81c3a06decaca11
zahraa-khalil <zahraa.khalil2020@gmail.com>
2024-07-17 00:20:52 +0300

    structure

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "search_algos.h"
+
+/**
+ * main - function that searches for a value in an array
+ * of integers using the Linear search algorithm
+ * Return: -1 If value is not present or if array is NULL
+ */
+int main(void)
+{
+    int array[] = {
+        10, 1, 42, 3, 4, 42, 6, 7, -1, 9
+    };
+    size_t size = sizeof(array) / sizeof(array[0]);
+
+    printf("Found %d at index: %d\n\n", 3, linear_search(array, size, 3));
+    printf("Found %d at index: %d\n\n", 42, linear_search(array, size, 42));
+    printf("Found %d at index: %d\n", 999, linear_search(array, size, 999));
+    return (EXIT_SUCCESS);
+}
---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,10 @@
+#ifndef FUNCTION_POINTER_H
+#define FUNCTION_POINTER_H
+#include <stddef.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+int int_index(int *array, int size, int (*cmp)(int));
+
+
+#endif
