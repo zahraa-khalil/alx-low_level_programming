6589336b170906f35d6853704249c6f716fa3f01
zahraa-khalil <zahraa.khalil2020@gmail.com>
2024-07-18 21:04:30 +0300

    update

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,6 +1,8 @@
 #ifndef FUNCTION_POINTER_H
 #define FUNCTION_POINTER_H
 #include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
